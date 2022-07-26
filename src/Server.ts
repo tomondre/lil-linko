c715f81dbcea33ba273ae7cee12f4815e6c88486
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-07-26 22:26:35 +0200

    Refactor and changing pointer state on the index page

--- src/Server.ts
+++ src/Server.ts
@@ -1,14 +1,5 @@
 import MainRouter from "./routes/MainRouter";
 import Express from "express";
-import IndexController from "./controller/IndexController";
-import {ILinkRepository} from "./repository/link/ILinkRepository";
-import LinkRepository from "./repository/link/LinkRepostiory";
-import LinkService from "./service/link/LinkService";
-import {ILinkService} from "./service/link/ILinkService";
-import LinkRoutes from "./routes/LinkRouter";
-import LinkController from "./controller/LinkController";
-import IndexRoutes from "./routes/IndexRouter";
-import { PrismaClient } from '@prisma/client'
 
 const app = Express();
 
